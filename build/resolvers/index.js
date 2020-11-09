760ef0c57441f6a9aa5dd6c6cb3649b6d24cba56
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2020-11-08 21:46:13 -0500

    production updated

--- build/resolvers/index.js
+++ build/resolvers/index.js
@@ -3,6 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var resolver_1 = __importDefault(require("./resolver"));
-exports.default = resolver_1.default;
+var userResolver_1 = __importDefault(require("./userResolver"));
+var categoryResolver_1 = __importDefault(require("./categoryResolver"));
+var recipeRespolver_1 = __importDefault(require("./recipeRespolver"));
+exports.default = [
+    userResolver_1.default,
+    categoryResolver_1.default,
+    recipeRespolver_1.default,
+];
 //# sourceMappingURL=index.js.map
--- build/resolvers/resolver.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var resolvers = {
-    Query: {
-        greetings: function () { return ('Hello'); }
-    }
-};
-exports.default = resolvers;
-//# sourceMappingURL=resolver.js.map
